fce3abd8a41f59e7563759ceb0ee074ef9b5d936
MathenP <112444824+MathenP@users.noreply.github.com>
2022-11-08 23:02:59 -0500

    Navbar Initial Commit

--- Controllers/Main.js
+++ Controllers/Main.js
@@ -5,5 +5,5 @@ const Item = require('mongoose').model('Item');
 const DBConn = require('../Config/Database');
 
 module.exports.RenderPage_Home = (req, res, next) => {
-    res.render('Home', { PageTitle: 'Home' });
+    res.render('Page', { PageTitle: 'Home' });
 }
