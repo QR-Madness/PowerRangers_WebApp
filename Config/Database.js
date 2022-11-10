471417daf97d66ca66df2d1961708f508ba669bc
Redacted <Redacted>
2022-11-09 19:35:10 -0500

    added connnection for Azure Cosmos DB

--- Config/Database.js
+++ Config/Database.js
@@ -11,5 +11,5 @@ const Mongo_DBConnection = mongoose.connect(MongoDB_Config.db);
 module.exports = Mongo_DBConnection;
 */
 module.exports = {
-    "URI": "mongodb://localhost/incident_management"
+    "URI": "mongodb://powerrangers:SNCHFJS71IQeS1t3NIkcmIpJyzThwxGYfzExGPsMAu4L5V4lTK8TBXHm6xFtg7EPIbRgU4EtpFmZACDbqDHpUg==@powerrangers.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@powerrangers@"
 }
