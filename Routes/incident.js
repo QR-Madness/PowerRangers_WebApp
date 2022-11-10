04325b7b737338337192682e9ace2f030187054c
Redacted <Redacted>
2022-11-09 19:10:51 -0500

    merged crud into dev

--- Routes/incident.js
+++ Routes/incident.js
@@ -3,18 +3,30 @@ let router = express.Router();
 let mongoose = require('mongoose');
 
 //connect to our incident model
-let Incident = require('../Models/Incident');
+let incident = require('../Models/Incident');
+let incidentController = require("../Controllers/Incident");
 
 //get route for the incident page - read operation
 
-router.get('/', (req, res, next) => {
-    Incident.find((err, incidentList) => {
-        if (err) {
-            return console.error(err);
-        } else {
-            res.render('incident.ejs', { PageTitle: 'Incident List', IncidentList: incidentList })
-        }
-    });
-});
+router.get('/', incidentController.displayIncidentList);
+
+//get route for displaying add page - Create operation
+router.get('/add', incidentController.displayAddPage);
+
+//get route for processing add page - Create operation
+router.post('/add', incidentController.processAddPage);
+
+// GET Route for displaying Edit page - UPDATE Operation 
+
+router.get('/edit/:id', incidentController.displayEditPage);
+
+// POST Route for processing Edit page - UPDATE Operation 
+
+router.post('/edit/:id', incidentController.processEditPage);
+
+// GET Route to perform Deletion - DELETE Operation 
+
+router.get('/delete/:id', incidentController.performDelete);
+
 
 module.exports = router;
