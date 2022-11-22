bd80860b4c1ec5862779b654c174e8bbd7e534a1
Rupinder Singh <is.rupinder23@gmail.com>
2022-11-22 11:18:50 -0500

    auth commit

--- Routes/incident.js
+++ Routes/incident.js
@@ -2,45 +2,31 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
-let passport = require('passport');
-
-let incidentController = require("../Controllers/Incident");
-
-// helper function for guard purposes
-function requireAuth(req, res, next)
-{
-    // check if the user is logged in
-    if(!req.isAuthenticated())
-    {
-        return res.redirect('/login');
-    }
-    next();
-}
 //connect to our incident model
-//let incident = require('../Models/Incident');
-
+let incident = require('../Models/Incident');
+let incidentController = require("../Controllers/Incident");
 
 //get route for the incident page - read operation
 
 router.get('/', incidentController.displayIncidentList);
 
 //get route for displaying add page - Create operation
-router.get('/add', requireAuth, incidentController.displayAddPage);
+router.get('/add', incidentController.displayAddPage);
 
 //get route for processing add page - Create operation
-router.post('/add', requireAuth, incidentController.processAddPage);
+router.post('/add', incidentController.processAddPage);
 
 // GET Route for displaying Edit page - UPDATE Operation 
 
-router.get('/edit/:id', requireAuth, incidentController.displayEditPage);
+router.get('/edit/:id', incidentController.displayEditPage);
 
 // POST Route for processing Edit page - UPDATE Operation 
 
-router.post('/edit/:id', requireAuth, incidentController.processEditPage);
+router.post('/edit/:id', incidentController.processEditPage);
 
 // GET Route to perform Deletion - DELETE Operation 
 
-router.get('/delete/:id', requireAuth, incidentController.performDelete);
+router.get('/delete/:id', incidentController.performDelete);
 
 
 module.exports = router;
